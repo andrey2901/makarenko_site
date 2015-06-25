de66c51f379f6cfa2ebb2011527a04a9ff091120
Andrey Vinnik <andrey2901@ua.fm>
2015-06-25 18:04:37 +0300

    Switch off prices' page.

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
 		<ul>
 			<li><a href="index.php">Главная</a></li>
 			<li><a href="services.php">Услуги</a></li>
-			<li><a href="prices.php">Цены</a></li>
+			<!-- <li><a href="prices.php">Цены</a></li> -->
 			<li><a href="comments.php">Отзывы</a></li>
 			<li><a href="contacts.php">Контакты</a></li>
 			<li><a href="about.php">О нас</a></li>
